42129f2b5898f9a51e91fcc0d090af777a853294
Georgi Yordanov <aaster23@protonmail.com>
2018-12-09 11:38:33 +0200

    Added email purchasing

--- Contact.js
+++ Contact.js
@@ -1,24 +1,9 @@
-//Gosho, delete all the code is this file and put your code here.
-
-//Example 1
-function ChangeText() {
-    document.getElementById("demo").innerHTML = "Hello World";
-}
-function ChangeStyle() {
-  for (var i = 0; i < 100; i++) {
-    document.getElementById("style").style.fontSize="50px"+i;
-  }
-}
-
-
-function numberValidation() {
-    var x, text;
-    x = document.getElementById("number").value;
-    if (x<1||x>10) {
-        text= "Input not valid";
-    }
-    else {
-      text = "Input valid";
-    }
-    document.getElementById("num").innerHTML=text;
+const sendEmail = () => {
+  const body = document.getElementById('text').value;
+  const subject = document.getElementById('subject').value;
+  const link = 'mailto:test@example.com?subject=subject&body=body';
+  console.log('asd')
+  window.location.href = link;
+  alert('Thanks for your purchase!');
+  // window.location.reload();
 }
