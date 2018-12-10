ba02c868668a20d4bf1dd1eacf345462ddae1a63
aaster23 <aaster23@protonmail.com>
2018-12-10 15:28:02 +0200

    Removed console.log('asd')

--- Contact.js
+++ Contact.js
@@ -2,7 +2,6 @@ const sendEmail = () => {
   const body = document.getElementById('text').value;
   const subject = document.getElementById('subject').value;
   const link = 'mailto:lachezar_dimitrov@outlook.com?subject=subject&body=body';
-  console.log('asd')
   window.location.href = link;
   alert('Thanks for your purchase!');
   // window.location.reload();
