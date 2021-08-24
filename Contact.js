a42bd6251503b9a9e8b05c45929934e6d8f5a24a
lachezar.dimitrov <lachezar.dimitrov@trading212.com>
2021-08-24 18:20:12 +0300

    Remove useless files

--- Contact.js
+++
@@ -1,8 +0,0 @@
-const sendEmail = () => {
-  const body = document.getElementById('text').value;
-  const subject = document.getElementById('subject').value;
-  const link = 'mailto:lachezar_dimitrov@outlook.com?subject=subject&body=body';
-  window.location.href = link;
-  alert('Thanks for your purchase!');
-  // window.location.reload();
-}
